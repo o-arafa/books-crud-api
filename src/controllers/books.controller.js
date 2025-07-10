0a0847a9b2c93396c80d07f4a7c8ea3bd52a3ea1
Obeida Arafa <obeidaarafa@gmail.com>
2025-07-10 23:06:31 +0300

    finish get single book and delete book

--- src/controllers/books.controller.js
+++ src/controllers/books.controller.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const Book = require("../models/book.model");
 const { bookSchema } = require("../validators/book.validator");
 
@@ -5,6 +6,36 @@ const getAllBooks = async (req, res) => {
   try {
     const books = await Book.find();
     res.status(200).json(books);
+  } catch (err) {
+    res.status(500).json({ message: "Server Error", error: err.message });
+  }
+};
+
+const getBook = async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.bookId)) {
+      return res.status(400).json({ message: "Invalid book ID" });
+    }
+    const book = await Book.findById(req.params.bookId);
+    if (!book) {
+      return res.status(404).json({ message: "book not found" });
+    }
+    res.status(200).json(book);
+  } catch (err) {
+    res.status(500).json({ message: "Server Error", error: err.message });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.bookId)) {
+      return res.status(400).json({ message: "Invalid book ID" });
+    }
+    const deleteBook = await Book.findByIdAndDelete(req.params.bookId);
+    if (!deleteBook) {
+      return res.status(404).json({ message: "book not found" });
+    }
+    res.status(200).json({ message: "book has deleted" });
   } catch (err) {
     res.status(500).json({ message: "Server Error", error: err.message });
   }
@@ -27,5 +58,7 @@ const createBook = async (req, res) => {
 
 module.exports = {
   getAllBooks,
+  getBook,
   createBook,
+  deleteBook,
 };
--- src/routes/books.route.js
+++ src/routes/books.route.js
@@ -4,5 +4,7 @@ const booksController = require("../controllers/books.controller");
 
 router.get("/", booksController.getAllBooks);
 router.post("/", booksController.createBook);
+router.get("/:bookId", booksController.getBook);
+router.delete("/:bookId", booksController.deleteBook);
 
 module.exports = router;
